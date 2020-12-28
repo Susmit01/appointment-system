ee2f1f9dc0f8b442b4872389183230bf106e8f6c
Susmita Mishra <heysusmita@gmail.com>
2020-12-28 18:35:42 +0530

    all files added

---
+++ aboutus.php
@@ -0,0 +1,61 @@
+<?php
+include("header.php");
+?>
+<div class="wrapper col2">
+  <div id="gallery">
+    <ul>
+      <li class="placeholder" style="background-image:url(images/demo/doctors.jpeg);">Image Holder</li>
+      <li><a class="swap" style="background-image:url(images/demo/infra.jpeg);" href="#gallery"><strong>Services</strong><span><img src="images/demo/image1.jpeg" alt="" /></span></a></li>
+      <li><a class="swap" style="background-image:url(images/demo/hosp.jpeg);" href="#gallery"><strong>Products</strong><span><img src="images/demo/shr_290913_hospital2.jpg" alt="" /></span></a></li>
+      <li class="last"><a class="swap" style="background-image:url(images/img1.jpeg);" href="#gallery"><strong>Company</strong><span><img src="images/demo/shr_290913_hospital5.jpg" alt="" /></span></a></li>
+    </ul>
+    <div class="clear"></div>
+  </div>
+</div>
+<div class="wrapper col4">
+  <div id="container">
+    <div id="content">
+      <h1>About XYZ Hospital</h1>
+      <p align="justify">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam hendrerit nisi sed sollicitudin pellentesque. Nunc posuere purus rhoncus pulvinar aliquam. Ut aliquet tristique nisl vitae volutpat. Nulla aliquet porttitor venenatis. Donec a dui et dui fringilla consectetur id nec massa. Aliquam erat volutpat. Sed ut dui ut lacus dictum fermentum vel tincidunt neque. Sed sed lacinia lectus. Duis sit amet sodales felis. Duis nunc eros, mattis at dui ac, convallis semper risus. In adipiscing ultrices tellus, in suscipit massa vehicula eu.</p>
+      <p align="justify">Rrow itself, let it be sorrow; let him love it; let him pursue it, ishing for its acquisitiendum. Because he will ab hold, uniess but through concer, an.</p>
+      
+      <div class="homecontent">
+        <ul>
+          <li>
+           <h2 align="center">Super Specialty Hospital</h2>
+            <p class="imgholder"><img src="images/speciality.jpg" alt="" style="width:286px;height:100px;"  /></p>
+          </li>
+          <li class="last">
+            <h2 align="center">24X7 services</h2>
+            <p class="imgholder"><img src="images/247.jpg" alt="" style="width:286px;height:100px;"   /></p>
+          </li>
+        </ul>
+        <div class="clear"></div>
+      </div>
+      <p><strong>Directors message:</strong><br />
+      Twho may be, let him love fellows of a polecat. Now amour, the, twhose being, drunk, yet twhitch and, an enclosed valleys always a laugh. In acquisitiendum the Furies are Earth; in (he takes up) a lump vehicles bien.</p>
+    </div>
+    <div id="column">
+      <div id="featured">
+        <ul>
+          <li>
+            <h2>Our Services</h2>
+            <p class="imgholder"><img src="images/demo/SDM-Image-5.jpg" alt="" style="width:240px;height:90px;" /></p>
+          </li>
+          <li role="menuitem"><a href=""  tabindex="-1">XYZ Hospital</li>
+          <li aria-haspopup="Sidebar1_Menu2:submenu:36" role="menuitem"><a href=""  tabindex="-1">Diagnostic Services</li>
+          <li role="menuitem"><a href=""  tabindex="-1">Consultation</li>
+          <li role="menuitem"><a href=""  tabindex="-1">DIn Patient Services</li>
+          <li role="menuitem"><a href=""  tabindex="-1">DHealth Checkup Packages</li>
+          <li role="menuitem"><a href=""  tabindex="-1">D24 X 7 Services</li>
+          <li role="menuitem"><a href=""  tabindex="-1">DPhysiotherapy Services</li>
+          
+        </ul>
+      </div>
+    </div>
+    <div class="clear"></div>
+  </div>
+</div>
+<?php
+include("footer.php");
+?>
---
+++ footer.php
@@ -0,0 +1,13 @@
+<link rel="stylesheet" href="layout/styles/layout.css" type="text/css" />
+  <div id="footer" style="background-color:#83b0eb;">
+
+    <div id="copyright"  style="background-color:#83b0eb;">
+      <p class="fl_left">Copyright &copy; 2020 - All Rights Reserved | <a href="adminlogin.php">Admin Login Panel</a> | <a href="doctorlogin.php">Doctor Login Panel</a></p>
+      <p class="fl_right">Developed by <a href="#">Mitali & Susmita</a></p>
+      <br class="clear" />
+    </div>
+    <div class="clear"></div>
+  </div>
+</div>
+</body>
+</html>
